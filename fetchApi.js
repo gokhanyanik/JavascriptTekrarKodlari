9ae90c6f5b95a0c5acff76891b92440a72a9f692
gokhanyanik <gokhanyanik@gmail.com>
2024-07-17 23:02:46 +0300

    FetchApi PUT examples eklendi

--- fetchApi.js
+++ fetchApi.js
@@ -25,3 +25,21 @@ fetch("https://jsonplaceholder.typicode.com/posts", {
     .then(response => response.json())
     .then(json => console.log(json))
     .catch(err => console.log(err))
+
+    // put işlemi 
+
+    let payload2={
+        id:1,
+        title:'fooo',
+        body:'bar',
+        userId:1,
+    }
+    fetch("https://jsonplaceholder.typicode.com/posts/1",{
+        method:"PUT",
+        body:JSON.stringify(payload2),   // put edilecek verinin tipini değiştirdik.
+        headers:{"Content-type": "application/json;charset=UTF-8"}  // gönderilecek verinin tipini belirttik.
+    })
+
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err))
